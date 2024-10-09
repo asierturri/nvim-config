4b5a0bc32a858abe22305661d600db5a6ab4f8c1
asierturri <asierturrillo@gmail.com>
2024-10-09 11:01:36 +0200

    Fix matlab lsp and slight lualine adjustment

--- lua/l1zit/plugins/colorscheme.lua
+++ lua/l1zit/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-	"cocopon/iceberg.vim",
+	"bluz71/vim-moonfly-colors",
+	name = "moonfly",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme iceberg")
+		vim.cmd("colorscheme moonfly")
 	end,
 }
