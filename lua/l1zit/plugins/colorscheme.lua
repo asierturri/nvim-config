53e4c6a147072aed6c9495eba64aaf1da2dc255d
asierturri <asierturrillo@gmail.com>
2024-10-10 15:40:11 +0200

    Change some thingy's

--- lua/l1zit/plugins/colorscheme.lua
+++ lua/l1zit/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
-	"bluz71/vim-moonfly-colors",
-	name = "moonfly",
+	"ellisonleao/gruvbox.nvim",
+	name = "gruvbox",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme moonfly")
+		vim.cmd("colorscheme gruvbox")
 	end,
 }
