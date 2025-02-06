f8e4a99a2632e6a78052bc7d8a2afb8866e96d12
asierturri <asierturrillo@gmail.com>
2025-02-06 14:17:21 +0100

    Fix and enhance new config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,17 @@
 return {
     "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
     config = function()
         require("mason").setup()
+        require("mason-lspconfig").setup({
+            automatic_installation = true,
+            ensure_installed = {
+                "clangd",
+                "matlab_ls",
+            },
+        })
     end,
 }
