c1af347ab325d0a947d2d3c8d4c1997d87d40ddb
L1ZiT <asierturrillo@gmail.com>
2025-02-06 14:17:21 +0100

    Fix and enhance new config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,33 @@
 local function on_attach(client, bufnr)
 	local opts = { noremap = true, silent = true }
 	local buf_set_keymap = vim.api.nvim_buf_set_keymap
+
+    buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config["matlab_ls"] = { 
+    cmd = { "matlab-language-server", "--stdio" },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "matlab" },
+    root_markers = { ".git", "*.prj", "*.m" },
+    settings = {
+        MATLAB = {
+            indexWorkspace = true,
+            installPath = "C:/Program Files/MATLAB/R2024b",
+            matlabConnectionTiming = "onStart",
+            telemetry = true,
+        },
+    },
+};
+vim.lsp.enable("matlab_ls")
+
+vim.diagnostic.config({
+    virtual_text = true,
+    underline = true,
+    signs = true,
+    update_in_insert = false,
+})
